dbaf9c5369be272b92ccc39e57958b98aae871b1
typeofname <dalfiannur@gmail.com>
2019-11-07 10:26:13 +0700

    New Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,204 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState, useCallback } from "react"
+import SEO from '../components/seo'
+import { Grid, Paper, TextField, FormControl, Select, MenuItem, InputLabel, Button, Snackbar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const URL = 'https://barcode-attendance-system.herokuapp.com'
+const useStyle = makeStyles(theme => ({
+  Paper: {
+    padding: 10
+  },
+  ButtonSubmit: {
+    margin: '10px auto',
+    display: 'block'
+  }
+}))
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const Departements = [
+  {
+    id: 1,
+    name: 'WH RPMP'
+  },
+  {
+    id: 2,
+    name: 'WH FG'
+  },
+  {
+    id: 3,
+    name: 'PPPC & PREPARASI PLANT'
+  },
+  {
+    id: 4,
+    name: 'PRODUKSI'
+  },
+  {
+    id: 5,
+    name: 'MAINTENANCE'
+  },
+  {
+    id: 6,
+    name: 'PPIC'
+  },
+  {
+    id: 7,
+    name: 'PROCUREMENT'
+  },
+  {
+    id: 8,
+    name: 'TPP'
+  },
+  {
+    id: 9,
+    name: 'PMD'
+  },
+  {
+    id: 10,
+    name: 'IT'
+  },
+  {
+    id: 11,
+    name: 'COSTING'
+  },
+  {
+    id: 12,
+    name: 'MS'
+  },
+]
+
+const Countries = [
+  {
+    value: 'indonesia',
+    text: 'Indonesia'
+  }
+]
+
+const IndexPage = () => {
+  const classes = useStyle()
+
+  const [valid, setValid] = useState(false)
+  const [success, setSuccess] = useState(false)
+  const [registered, setRegistered] = useState(true)
+  const [nik, setNIK] = useState('')
+  const [name, setName] = useState('')
+  const [departement, setDepartement] = useState('')
+  const [country, setCountry] = useState(null)
+  const [picture, setPicture] = useState([])
+
+  const onSubmit = useCallback(() => {
+    const body = new FormData()
+    body.append('nik', nik)
+    body.append('name', name)
+    body.append('depardement', departement)
+    body.append('country', country)
+    body.append('picture', picture[0])
+
+    console.log(picture)
+
+    fetch(`${URL}/register`, {
+      method: 'POST',
+      body
+    })
+      .then(() => {
+        console.log('success')
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [nik, name, departement, country])
+
+  const checkNIK = useCallback(() => {
+    fetch(`${URL}/nik/${nik}`)
+      .then(result => result.json())
+      .then(({ exist }) => {
+        if (!exist) {
+          setRegistered(true)
+          setValid(true)
+        } else {
+          setRegistered(false)
+          setValid(false)
+        }
+      })
+  }, [nik])
+
+  return (
+    <React.Fragment>
+      <SEO title="Register" />
+      <Grid container justify='center'>
+        <Grid md={4}>
+          <Paper className={classes.Paper}>
+            <FormControl
+              fullWidth
+              margin='normal'>
+              <TextField
+                error={!valid}
+                fullWidth
+                onChange={e => setNIK(e.target.value)}
+                label='NIK'
+                onKeyUp={checkNIK}
+                helperText={registered ? '' : 'NIK has been registered'} />
+            </FormControl>
+            <FormControl
+              fullWidth
+              margin='normal'>
+              <TextField
+                fullWidth
+                onChange={e => setName(e.target.value)}
+                label='Name' />
+            </FormControl>
+            <FormControl
+              fullWidth
+              margin='normal'>
+              <InputLabel id="input-departement-label">Departement</InputLabel>
+              <Select
+                labelId="input-departement-label"
+                id="input-departement"
+                value={departement}
+                fullWidth
+                onChange={e => setDepartement(e.target.value)}
+              >
+                {
+                  Departements.map(item => (
+                    <MenuItem value={item.id} key={item.name}>{item.name}</MenuItem>
+                  ))
+                }
+              </Select>
+            </FormControl>
+            <FormControl
+              fullWidth
+              margin='normal'>
+              <InputLabel id="input-country-label">Country</InputLabel>
+              <Select
+                labelId="input-country-label"
+                id="input-country"
+                value={country}
+                fullWidth
+                onChange={e => setCountry(e.target.value)}
+              >
+                {
+                  Countries.map(item => (
+                    <MenuItem value={item.value} key={item.value}>{item.text}</MenuItem>
+                  ))
+                }
+              </Select>
+            </FormControl>
+            <input type='file' onChange={e => setPicture(e.target)} />
+            <Button color='primary' variant='contained' className={classes.ButtonSubmit} disabled={!valid} onClick={onSubmit}>
+              Submint
+            </Button>
+          </Paper>
+        </Grid>
+      </Grid>
+      <Snackbar
+        open={success}
+        message={<p>User successfully registered</p>}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        autoHideDuration={6000}
+        onClose={() => setSuccess(false)} />
+    </React.Fragment>
+  )
+}
 
 export default IndexPage
